a96d7e1ef14567edb52ee56554901746d073a710
Makar Betlei <makar.betlei@etu.univ-grenoble-alpes.fr>
2024-07-01 17:17:44 +0200

    CMakeLists.txt file was created, also few changes in main.cpp

--- main.cpp
+++ main.cpp
@@ -23,6 +23,11 @@ struct Atom {
     float center_z;
 };
 
+bool is_empty(std::ifstream& pFile)
+{
+    return pFile.peek() == std::ifstream::traits_type::eof();
+}
+
 float calculateDistance(Atom a1, Atom a2) {
     return std::sqrt(std::pow(a2.center_x - a1.center_x, 2) + std::pow(a2.center_y - a1.center_y, 2) + std::pow(a2.center_z - a1.center_z, 2));
 }
@@ -77,30 +82,56 @@ std::map< ResidueID, std::vector< Atom > > groupAtoms(const std::vector<Atom>& a
     return groupedAtoms;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     try {
-        std::string filename;
-        std::cout << "Enter the filename: ";
-        std::cin >> filename;
+        // std::string filename;
+        // std::cout << "Enter the filename: ";
+        // std::cin >> filename;
 
-        std::ifstream file(filename);
-        if (!file.is_open()) {
-            throw std::runtime_error("Unable to open file: " + filename);
+        // std::ifstream file(filename);
+        // if (!file.is_open()) {
+        //     throw std::runtime_error("Unable to open file: " + filename);
+        // }
+        if (argc < 2) {
+            std::cerr << "Usage: " << argv[0] << " <filename>\n";
+            return 1;
+        }
+
+        std::string file_name = argv[1];
+
+        // Open the file in read mode
+        std::ifstream f(file_name);
+
+        if (!f) {
+            throw std::runtime_error("Unable to open file: " + file_name);
+        }
+        if (is_empty(f)) {
+            throw std::runtime_error("The file " + file_name + "is empty!");
         }
 
         std::vector< std::string > headers;
-        std::vector< Atom > atoms = getAtomsFromTSV(file, headers);
-        file.close();
+        std::vector< Atom > atoms = getAtomsFromTSV(f, headers);
+        f.close();
 
         std::map< ResidueID, std::vector< Atom > > groupedAtoms = groupAtoms(atoms);
-        for (const auto& entry : groupedAtoms) {
-        ResidueID key = entry.first;
-        const std::vector<Atom>& value = entry.second;
-        std::cout << "Key: (" << key.chainID << ", " << key.resSeq << ")\n";
-        for (const Atom& atom : value) {
-            std::cout << "  Atom: { name " << atom.atom_name << ", center_x: " << atom.center_x 
-            << ", center_y: " << atom.center_y << ", center_z: " << atom.center_z << " }\n";
-        }
+        typedef std::map< ResidueID, std::vector< Atom > >::const_iterator GroupIterator;
+        for (GroupIterator it1 = groupedAtoms.begin(); it1 != groupedAtoms.end(); ++it1) {
+            for (GroupIterator it2 = std::next(it1); it2 != groupedAtoms.end(); ++it2) {
+                if (it1->first.chainID == it2->first.chainID) {
+                    const std::vector<Atom>& atoms1 = it1->second;
+                    const std::vector<Atom>& atoms2 = it2->second;
+
+                    for (const Atom& atom1 : atoms1) {
+                        for (const Atom& atom2 : atoms2) {
+                            float distance = calculateDistance(atom1, atom2);
+                            std::cout << "Distance between (" << it1->first.chainID << ", " << it1->first.resSeq 
+                                      << ") and (" << it2->first.chainID << ", " << it2->first.resSeq 
+                                      << ") atoms " << atom1.atom_name << " and " << atom2.atom_name 
+                                      << " is: " << distance << "\n";
+                        }
+                    }
+                }
+            }
         }
 
 
