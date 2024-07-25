10812389aca505354f0c98f3b6490e6be346450c
Makar Betlei <makar.betlei@etu.univ-grenoble-alpes.fr>
2024-07-25 16:18:32 +0200

    get_final_table.r: provided generation of tables of type "sep2"

--- scripts/get_final_table.r
+++ scripts/get_final_table.r
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+
 # Reading arguments from the command line
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -38,4 +40,33 @@ merged_data <- merge(filtered_skempi, z_scores_data, by = c("ID_chainID", "ID_re
 # Saving the result to a new file
 output_file <- paste0(folder_name, "/", pdb_id, "/", pdb_id, ".tsv")
 write.table(merged_data, file = output_file, sep = "\t", row.names = FALSE, quote = FALSE)
-cat("Результат сохранен в файл:", output_file, "\n")
+
+
+
+file_skempi <- "input/SKEMPI/skempi_v2_single_mutations_with_ddG.tsv"
+file_z_scores <- paste0(folder_name, "/", pdb_id, "/sep2_z_scores_centrality_measures.tsv")
+
+# Checking the existence of files
+if (!file.exists(file_skempi)) {
+  stop(paste("File not found:", file_skempi))
+}
+if (!file.exists(file_z_scores)) {
+  stop(paste("File not found:", file_z_scores))
+}
+
+# Reading of the tables
+skempi_data <- read.delim(file_skempi, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+z_scores_data <- read.delim(file_z_scores, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+
+# Filtering of the first table by pdb_id
+filtered_skempi <- skempi_data[skempi_data$pdb_id == pdb_id, ]
+
+# Renaming columns in the z_scores_data table
+colnames(filtered_skempi)[3:4] <- c("ID_chainID", "ID_resSeq")
+
+# Merge tables by “ID_chainID” and “ID_resSeq” columns
+merged_data <- merge(filtered_skempi, z_scores_data, by = c("ID_chainID", "ID_resSeq"))
+
+# Saving the result to a new file
+output_file <- paste0(folder_name, "/", pdb_id, "/sep2_", pdb_id, ".tsv")
+write.table(merged_data, file = output_file, sep = "\t", row.names = FALSE, quote = FALSE)
